ac01c251429697c65e287e376e76dfb9cea9c56f
Oct-Zhan <1504720188@qq.com>
2022-04-20 16:10:40 +0800

    辛网视动m first commit

---
+++ 辛网视动-m/js/index.js
@@ -0,0 +1,12 @@
+   $(function(){
+	    // 自适应
+	     $(window).resize(infinite);
+	     function infinite(){
+	         let htmlWidth = $('html').width()
+	         if(htmlWidth <= 750){
+	             $('html').css('font-size', 30 / 750 * htmlWidth + 'px')
+	         }else{
+	            $('html').css('font-size', '30px')
+	         }
+	     }infinite()
+	})
